0e33909a551cf0164b74bbfe2a7771feb70cf40f
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-06 04:43:35 -0800

    Update swiperaction.js

--- js/swiperaction.js
+++ js/swiperaction.js
@@ -1,6 +1,10 @@
     var swiper = new Swiper('.swiper-container', {
       // Enable lazy loading
       lazy: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },  
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
