284b080dfe07d1682b78d2377678394cb953d125
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-12 02:50:26 -0800

    Update datacollection.js

--- js/datacollection.js
+++ js/datacollection.js
@@ -1,6 +1,19 @@
-   const api = 'https://corona-api.com/';
+const globalapi1 = 'https://corona-api.com/countries';
+const globalapi2 = 'https://api.covid19api.com/summary';
 
-   fetch(api)
+const localapi1 = 'https://api.covidtracking.com/v1/us/current.json';
+
+
+//Global Count from about-corona
+   fetch(globalapi1)
+  .then(response => {
+   return response.json();
+  })
+  .then(data => {
+   console.log(data);
+     
+//Global Count from covid19api      
+   fetch(globalapi2)
   .then(response => {
    return response.json();
   })
