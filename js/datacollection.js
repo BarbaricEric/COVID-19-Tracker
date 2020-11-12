6f800083bccabd4d34d81a371779320a59840aae
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-12 05:23:48 -0800

    Update datacollection.js

--- js/datacollection.js
+++ js/datacollection.js
@@ -1,16 +1,26 @@
+//DOM Elements
+const usCaseDeath = document.querySelector(".us-case-death");
+const usCaseConfirm = document.querySelector(".us-case-confirmed");
+
+//Define API
 const globalapi1 = 'https://corona-api.com/countries';
 const globalapi2 = 'https://api.covid19api.com/summary';
-
 const localapi1 = 'https://api.covidtracking.com/v1/us/current.json';
+const localapi2 = 'https://corona-api.com/countries/US';
 
 
-//Global Count from about-corona
-   fetch(globalapi1)
+//US Count from about-corona
+   fetch(localapi2)
   .then(response => {
    return response.json();
   })
   .then(data => {
-   console.log(data);
+   console.log(data1);
+   const { name, population, updated_at, deaths, confirmed, cases_per_million_population } = data1;
+   //Set DOM element from API
+   usCaseDeath.textContent = deaths;
+   usCaseConfirm.textContent = confirmed;
+   });      
      
 //Global Count from covid19api      
    fetch(globalapi2)
@@ -18,4 +28,5 @@ const localapi1 = 'https://api.covidtracking.com/v1/us/current.json';
    return response.json();
   })
   .then(data => {
-   console.log(data);
+   console.log(data2);
+   });      
