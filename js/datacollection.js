5a9fcce798df4a62ae14ad6097d033a2998f785d
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-12 07:39:50 -0800

    Update datacollection.js

--- js/datacollection.js
+++ js/datacollection.js
@@ -18,8 +18,8 @@ const localapi2 = 'https://corona-api.com/countries/US';
    console.log(data);
    const { name, population, updated_at, deaths, confirmed, cases_per_million_population } = data;
    //Set DOM element from API
-   usCaseDeath.textContent = data.deaths;
-   usCaseConfirm.textContent = data.confirmed;
+   usCaseDeath.textContent = data.latest_data.deaths;
+   usCaseConfirm.textContent = data.latest_data.confirmed;
    });      
      
 //Global Count from covid19api      
