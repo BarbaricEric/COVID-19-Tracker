3dfa6b31a945fd11dbedb414d44eb0863ba4d09b
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-18 04:33:51 -0800

    Create globaldatacollection.js

---
+++ js/globaldatacollection.js
@@ -0,0 +1,34 @@
+//DOM Elements
+const usCaseDeath = document.querySelector(".us-case-death");
+const usCaseConfirm = document.querySelector(".us-case-confirmed");
+
+//Define API
+const globalapi1 = 'https://corona-api.com/countries';
+const globalapi2 = 'https://api.covid19api.com/summary';
+
+//Global Count from covid19api      
+   fetch(globalapi2)
+  .then(response => {
+   return response.json();
+  })
+  .then(data => {
+   console.log(data);
+   usCaseDeath = getArrayFields(data.181, "TotalDeaths");
+   usCaseConfirm = getArrayFields(data.181, "TotalConfirmed");
+   });
+   
+//Global Count from corona-api      
+   fetch(globalapi1)
+  .then(response => {
+   return response.json();
+  })
+  .then(data => {
+   console.log(data);
+   });
+    
+   function getArrayFields(input, field) {
+    const output = [];
+    for (var i=0; i < input.length ; ++i)
+        output.push(input[i][field]);
+    return output;
+}
