67865b4dcbe7e4639c9bc27ed33650f8b5aed010
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-11 03:51:02 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -1,4 +1,21 @@
-chart = {
-  const width =
-  const height = 
-}
+//covid-api.com api
+let ustimeline = []
+let catimeline = []
+
+
+//US Count from covid-api.com    
+fetch('https://covid-api.com/api/reports?date=2020-03-01&iso=USA')
+  .then(response => {
+   return response.json();
+  })
+  .then(data => {
+   console.log(data);
+  });
+
+fetch('https://covid-api.com/api/reports?date=2020-03-02&iso=USA')
+  .then(response => {
+   return response.json();
+  })
+  .then(data => {
+   console.log(data);
+  });
