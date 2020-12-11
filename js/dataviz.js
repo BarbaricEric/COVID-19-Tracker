0345179e950ef2dedb079acb317a9aa581e8c52f
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-11 06:05:42 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -2,6 +2,12 @@
 let ustimeline = []
 let catimeline = []
 
+async getUsCountFromCovidApi() {
+ const fish = await fetch('https://covid-api.com/api/reports?date=2020-03-01&iso=USA').then(response => response.json());
+ const fish2 = await fetch('https://covid-api.com/api/reports?date=2020-03-02&iso=USA').then(response => response.json()); 
+}
+
+getUsCountFromCovidApi();
 
 //US Count from covid-api.com    
 fetch('https://covid-api.com/api/reports?date=2020-03-01&iso=USA')
