fd9573a4bf1881320520999a2d2ee4e0fd7d5e43
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-18 06:28:49 -0800

    Update datacollection.js

--- js/datacollection.js
+++ js/datacollection.js
@@ -50,7 +50,7 @@ const localapi2 = 'https://corona-api.com/countries/US';
    });
 
 //US Count from covid-api.com      
-   fetch('https://covid-api.com/api/reports?date=2020-12-16&iso=USA')
+   fetch('https://covid-api.com/api/reports?date=2020-12-17&iso=USA')
   .then(response => {
    return response.json();
   })
@@ -424,13 +424,13 @@ const localapi2 = 'https://corona-api.com/countries/US';
    const usCaseConfirm = document.querySelector(".us-newyorktimes-confirm");
    const usCaseDeath = document.querySelector(".us-newyorktimes-death");
    const usCaseRecent = document.querySelector(".us-newyorktimes-recent");
-   usCaseConfirm.textContent = data[330].cases;
-   usCaseDeath.textContent = data[330].deaths; 
-   usCaseRecent.textContent = data[330].date;
+   usCaseConfirm.textContent = data[331].cases;
+   usCaseDeath.textContent = data[331].deaths; 
+   usCaseRecent.textContent = data[331].date;
   });
 
 //NY Count from New York Times
-  d3.csv("./nytimes_covid_19_data/nytimes_daily_reports/12-14-2020.csv").then(function(data) {
+  d3.csv("./nytimes_covid_19_data/nytimes_daily_reports/12-16-2020.csv").then(function(data) {
    console.log(data);
    const alCaseConfirm = document.querySelector(".us-newyorktimes-al-confirm");
    const alCaseDeath = document.querySelector(".us-newyorktimes-al-death");
