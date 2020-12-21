074a0d271e92efcbcffb18b9e0f7a00e4318cba5
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-21 05:44:47 -0800

    Create globaldataviz.js

---
+++ js/globaldataviz.js
@@ -0,0 +1,35 @@
+chart = {
+  const svg = d3.select(DOM.svg(width, height))
+      .style("-webkit-tap-highlight-color", "transparent")
+      .style("overflow", "visible");
+
+  svg.append("g")
+      .call(xAxis);
+
+  svg.append("g")
+      .call(yAxis);
+  
+  svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+
+  const tooltip = svg.append("g");
+
+  svg.on("touchmove mousemove", function(event) {
+    const {date, value} = bisect(d3.pointer(event, this)[0]);
+
+    tooltip
+        .attr("transform", `translate(${x(date)},${y(value)})`)
+        .call(callout, `${formatValue(value)}
+${formatDate(date)}`);
+  });
+
+  svg.on("touchend mouseleave", () => tooltip.call(callout, null));
+
+  return svg.node();
+}
