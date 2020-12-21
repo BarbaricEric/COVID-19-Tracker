2d84f6ef79dff7b2db6f616a241d765ca778775f
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-21 07:33:44 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -59,6 +59,37 @@ let wvtimeline = []
 let witimeline = []
 let wytimeline = []
 
+const data = Object.assign(d3.csvParse(await FileAttachment("./nytimes_covid_19_data/nytimes_us_total.csv").text(), 
+  d3.autoType).map(({date, cases}) => ({date, cases: cases})), {y: "# of Cases"})
+  console.log(data);
+
+const line = d3.line()
+    .defined(d => !isNaN(d.cases))
+    .x(d => x(d.date))
+    .y(d => y(d.cases));
+
+const x = d3.scaleUtc()
+    .domain(d3.extent(data, d => d.date))
+    .range([margin.left, width - margin.right]);
+
+const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value)]).nice()
+    .range([height - margin.bottom, margin.top]);
+
+const xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+const yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text(data.y));
+
 async function getUsCountFromCovidApi() {
  const fish = await fetch('https://covid19-api.org/api/timeline/US').then(response => response.json())
   .then(data => {
