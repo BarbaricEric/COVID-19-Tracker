5b9d96297f42c53d3b3231200e9a0763d9430527
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-08 07:55:09 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -151,7 +151,7 @@ async function getUsCountFromCovidApi() {
 
 getUsCountFromCovidApi();
 
-//New York Times Chart
+//New York Times Total Chart
 Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows){
 
   function unpack(rows, key) {
@@ -160,7 +160,7 @@ Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows
 
   // header values
   var headerNames = Plotly.d3.keys(rows[0]);
-  var headerValues = [headerNames[1],headerNames[2],headerNames[3]];
+  var headerValues = [headerNames[0],headerNames[1],headerNames[2]];
 
   // cell values
   var cellValues = [];
@@ -178,7 +178,7 @@ Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows
   // create table
   var table = {
     type: 'table',
-    columnwidth: [300,300,300,300],
+    columnwidth: [500,500,500,500],
     columnorder: [0,1,2,3],
     header: {
       values: headerValues,
@@ -196,7 +196,7 @@ Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows
     },
     xaxis: 'x',
     yaxis: 'y',
-    domain: {x: [0,0.4], y: [0,1]}
+    domain: {x: [0,0.3], y: [0,1]}
   }
 
   // create 1st plot
@@ -219,37 +219,51 @@ Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows
     line: {width: 2, color: '#b04553'},
     name: 'Deaths'
   }
+  
+  // create 3rd plot
+  var trace3 = {
+    x: unpack(rows, 'date'),
+    y: unpack(rows, 'deaths'),
+    xaxis: 'x3',
+    yaxis: 'y3',
+    mode: 'lines',
+    line: {width: 2, color: '#af7bbd'},
+    name: 'Deaths'
+  }
 
-  var data = [table,trace1,trace2]
+  var data = [table,trace1,trace3]
 
   // define subplot axes
   var axis = {
     showline: true,
     zeroline: false,
-    showgrid: true,
+    showgrid: false,
     mirror:true,
     ticklen: 4,
     gridcolor: '#ffffff',
     tickfont: {size: 10},
   }
 
-  var axis1 = {domain: [0.5, 1], anchor: 'y1', showticklabels: false}
-  var axis2 = {domain: [0.5, 1], anchor: 'y2', showticklabels: false}
-  var axis4 = {domain: [0.66, 0.98], anchor: 'x1', hoverformat: '.2f'}
+  var axis1 = {domain: [0.35, 1], anchor: 'y1'}
+  var axis2 = {domain: [0.35, 1], anchor: 'y2', showticklabels: false}
+  var axis3 = {domain: [0.35, 1], anchor: 'y3'}
+  var axis4 = {domain: [0.52, 1], anchor: 'x1', hoverformat: '.2f'}
   var axis5 = {domain: [0.34, 0.64], anchor: 'x2', hoverformat: '.2f'}
+  var axis6 = {domain: [0.0, 0.42], anchor: 'x3', hoverformat: '.2f'}
 
   // define layout
   var layout = {
     title: "Total US COVID-19 Cases and Deaths",
-    plot_bgcolor: 'rgba(228, 222, 249, 0.65)',
+    plot_bgcolor: 'rgba(255, 255, 255, 1)',
     showlegend: false,
     xaxis1: Object.assign(axis1,axis),
     xaxis2: Object.assign(axis2,axis),
+    xaxis3: Object.assign(axis3,axis),
     yaxis1: Object.assign(axis4,axis),
-    yaxis2: Object.assign(axis5,axis)
+    yaxis2: Object.assign(axis5,axis),
+    yaxis3: Object.assign(axis6,axis)
   }
 
-  Plotly.newPlot('us-nytimes-chart', data, layout);
+  Plotly.newPlot('us-nytimes-chart', data, layout, {displayModeBar: false});
 
 });
-
