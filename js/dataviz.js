c8db1af2c656842048025d553301170605235c77
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-20 08:00:11 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -396,7 +396,7 @@ Highcharts.chart('us-covidtrack-average-chart', {
         title: {
             text: 'Date'
         },
-        categories: dateValue//categories: ['Jan 2019', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan 2020', '1/15/2021']
+        categories: [dateValue]//categories: ['Jan 2019', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan 2020', '1/15/2021']
     },
     
     credits: {
@@ -405,9 +405,9 @@ Highcharts.chart('us-covidtrack-average-chart', {
 
     series: [{
         name: "Cases",
-        data: posValue/*[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4,500.0,200.1]*/
+        data: [posValue]/*[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4,500.0,200.1]*/
     }, {
-        data: deathValue/*[216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5,500.4,2000.1]*/,
+        data: [deathValue]/*[216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5,500.4,2000.1]*/,
         name: "Deaths",
         lineWidth: 5
     }]
