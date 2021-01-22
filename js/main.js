c7d3e59e31f895a446c7124961cee071cb8d436c
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-22 06:46:09 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const browserSupport = document.querySelector('.banner-support');
-const bannerLogo = document.querySelector('.banner-logo');
 
 //Browser Support Banner
-setTimeout(() => {browserSupport.style.bottom = '-200px'; bannerLogo.style.bottom = '-200px';}, 8.0*1000);
+setTimeout(() => {browserSupport.style.display = 'none';}, 8.0*1000);
