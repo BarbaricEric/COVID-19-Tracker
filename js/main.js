1babf39b0d502f06dc5c651aaeac6a846ed37a3d
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-26 05:38:56 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 const browserSupport = document.querySelector('.banner-support');
-const p = document.querySelectorAll('tr');
 
 //Browser Support Banner
 setTimeout(() => {browserSupport.style.display = 'none';}, 8.0*1000);
-
-//Mouse Hover
-p.onmouseover = logMouseOver();
-
-function logMouseOver() {
-  p.style.backgroundColor = '#FFD700';
-  p.style.transition = 'background-color .5s';
-}
