90bfbe7c962c285fb02b63075cdb6f4246b25e58
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-26 05:39:38 -0800

    Update globalmain.js

--- js/globalmain.js
+++ js/globalmain.js
@@ -1,16 +1,7 @@
 const browserSupport = document.querySelector('.banner-support');
-const p = document.querySelector('tr');
 
 //Browser Support Banner
 setTimeout(() => {browserSupport.style.display = 'none';}, 8.0*1000);
-
-//Mouse Hover
-p.onmouseover = logMouseOver();
-
-function logMouseOver() {
-  p.style.backgroundColor = '#FFD700';
-  p.style.transition = 'background-color .5s';
-}
 
 if (document.documentElement.clientWidth < 480) { 
     document.querySelector("meta[name=viewport]").setAttribute(
