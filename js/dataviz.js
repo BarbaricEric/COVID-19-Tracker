17205fd3ca0f4ef1c048eddfcbcdda8b8fe06ea6
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-01-28 06:14:26 -0800

    Update dataviz.js

--- js/dataviz.js
+++ js/dataviz.js
@@ -257,6 +257,77 @@ async function getUsCountFromCovidApi() {
 
 getUsCountFromCovidApi();
 
+Plotly.d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'AAPL High',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'AAPL.High'),
+  line: {color: '#17BECF'}
+}
+
+var trace2 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'AAPL Low',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'AAPL.Low'),
+  line: {color: '#7F7F7F'}
+}
+
+var data = [trace1,trace2];
+
+var layout = {
+  title: 'Time Series with Rangeslider',
+  autosize: false,
+  width: 600,
+  height: 600,
+  xaxis: {
+    autorange: true,
+    title: 'Dates',
+    range: ['2015-02-17', '2017-02-16'],
+    rangeselector: {buttons: [
+        {
+          count: 1,
+          label: '1 Month',
+          step: 'month',
+          stepmode: 'backward'
+        },
+        {
+          count: 6,
+          label: '6 Month',
+          step: 'month',
+          stepmode: 'backward'
+        },
+        {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1 Year'
+        },
+        {step: 'all'}
+      ]},
+    rangeslider: {range: ['2015-02-17', '2017-02-16']},
+    type: 'date'
+  },
+  yaxis: {
+    autorange: true,
+    range: [86.8700008333, 138.870004167],
+    type: 'linear',
+    title: 'Number Values'
+  }
+};
+
+Plotly.newPlot('us-nytimes-average-chart', data, layout, {displayModeBar: false});
+})
+
 //New York Times Total Chart, Plotly
 Plotly.d3.csv("./nytimes_covid_19_data/nytimes_us_total.csv", function(err, rows){
 
