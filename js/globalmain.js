b0725b24656c502898a8f86cca90fad8a99b8d9e
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-02-02 05:10:21 -0800

    Update globalmain.js

--- js/globalmain.js
+++ js/globalmain.js
@@ -3,8 +3,16 @@ const browserSupport = document.querySelector('.banner-support');
 //Browser Support Banner
 setTimeout(() => {browserSupport.style.display = 'none';}, 8.0*1000);
 
-if (document.documentElement.clientWidth < 480) { 
-    document.querySelector("meta[name=viewport]").setAttribute(
-          'content', 
-          'width=device-width, height=device-height, initial-scale=0.25, maximum-scale=1.0, user-scalable=yes');
+if (document.documentElement.clientWidth < 480px) { 
+    const viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=0.25, maximum-scale=1.0, user-scalable=yes');
+ } else {
+  console.log('Error');
  }
+
+
+//viewport = document.querySelector("meta[name=viewport]");
+//viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+
+//const viewport = document.querySelector("meta[name=viewport]");
+//viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=0.25, maximum-scale=1.0, user-scalable=yes');
