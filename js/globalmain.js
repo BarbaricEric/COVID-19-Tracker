e39fc58f0ef6e19c6b344067225535ba41ed5d78
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-02-02 06:32:00 -0800

    Update globalmain.js

--- js/globalmain.js
+++ js/globalmain.js
@@ -11,8 +11,9 @@ if (document.documentElement.clientWidth < 480) {
  }
 
 
-//viewport = document.querySelector("meta[name=viewport]");
-//viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+//document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
 
-//const viewport = document.querySelector("meta[name=viewport]");
-//viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=0.25, maximum-scale=1.0, user-scalable=yes');
+//https://stackoverflow.com/questions/21419404/setting-the-viewport-to-scale-to-fit-both-width-and-height
+//https://stackoverflow.com/questions/1248081/how-to-get-the-browser-viewport-dimensions
+//https://stackoverflow.com/questions/37181403/how-to-set-browser-viewport-size
+//https://stackoverflow.com/questions/6942785/window-innerwidth-vs-document-documentelement-clientwidth
