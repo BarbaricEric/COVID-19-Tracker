6deefc86ce1fe1868354b27a6d871c25c2c4ef20
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-02-04 06:24:53 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 const browserSupport = document.querySelector('.banner-support');
+const covidtrackinfo = document.querySelector('.covidtrackupdate');
 
 //Browser Support Banner
 setTimeout(() => {browserSupport.style.display = 'none';}, 8.0*1000);
+
+//Covid Tracking Project Info
+p.onmouseover = logMouseOver;
+
+function logMouseOver() {
+  covidtrackinfo.style.color = '#1500ff';
+  covidtrackinfo.style.background = '#ffff00';
+}  
+
+covidtrackinfo.addEventListener('click', ()=>{
+	    const url = 'https://covidtracking.com/analysis-updates/covid-tracking-project-end-march-7';
+      const win = window.open(url, '_blank');
+})
