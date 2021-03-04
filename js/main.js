e6b5b3241ce9e21e81339244e7faecd461ee1d64
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-03-04 05:27:38 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ function step1() {
   direction: 'alternate',
   loop: false,
   easing: 'spring(1, 80, 10, 0)', //'linear',
-  delay: 5000,  
+  delay: 5000,
+  endDelay: 4000,		
   duration: 8000  
 });
 }
