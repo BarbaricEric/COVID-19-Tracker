528968ca93bf749925186e20e6855c152823be02
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-03-10 06:45:41 -0800

    Update globaldataviz.js

--- js/globaldataviz.js
+++ js/globaldataviz.js
@@ -158,9 +158,11 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
             .x(d => x(+d.time))
             .y(d => y(+d.value))
             .curve(d3.curveMonotoneX)
-   
           )
           .attr("stroke", function(d){ return myColor(selectedGroup) })
+          .call(mouseover)
+          .call(mousemove)
+          .call(mouseleave)
     }
 
     // When the button is changed, run the updateChart function
@@ -172,8 +174,117 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
     })
 
 })
-/*
+
 // set the dimensions and margins of the second graph
+var margin2 = {top: 10, right: 100, bottom: 30, left: 30},
+    width2 = 660 - margin2.left - margin2.right,
+    height2 = 600 - margin2.top - margin2.bottom;
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#test2")
+  .append("svg")
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin2.left + "," + margin2.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
+
+    // List of groups (here I have one group per column)
+    var allGroup = ["valueA", "valueB", "valueC"]
+
+    // add the options to the button
+    d3.select("#selectButton2")
+      .selectAll('myOptions')
+     	.data(allGroup)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    // A color scale: one color for each group
+    var myColor2 = d3.scaleOrdinal()
+      .domain(allGroup)
+      .range(d3.schemeSet2);
+
+    // Add X axis --> it is a date format
+    var x2 = d3.scaleLinear()
+      .domain([0,10])
+      .range([ 0, width2 ]);
+    svg2.append("g")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y2 = d3.scaleLinear()
+      .domain( [0,20])
+      .range([ height2, 0 ]);
+    svg2.append("g")
+      .call(d3.axisLeft(y));
+      
+    //Add Title
+    var title2 = svg2
+      .append("text")
+      .attr("x", (width2 / 2))   
+      .attr("y", margin2.top)
+      //.attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "30px") 
+      .style("text-decoration", "underline")  
+      .text("D3 Test2");
+      
+    // Initialize line with group a
+    var line2 = svg2
+      .append('g')
+      .append("path")
+        .datum(data)
+        .attr("d", d3.line()
+          //.x(function(d) { return x(+d.time) })
+          //.y(function(d) { return y(+d.valueA) })
+          .x(d => x(+d.time))
+          .y(d => y(+d.valueA))
+          .curve(d3.curveMonotoneX)    
+        )
+        .attr("stroke", function(d){ return myColor("valueA") })
+        .style("stroke-width", 4)
+        .style("fill", "none")
+
+    // A function that update the chart
+    function update(selectedGroup) {
+
+      // Create new data with the selection
+      var dataFilter = 
+          //data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+          data.map(d => ({time: d.time, value: d[selectedGroup]}))
+
+      // Give these new data to update line
+      line2
+          .datum(dataFilter)
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()    
+            //.x(function(d) { return x(+d.time) })
+            //.y(function(d) { return y(+d.value) })
+            .x(d => x(+d.time))
+            .y(d => y(+d.value))
+            .curve(d3.curveMonotoneX)
+          )
+          .attr("stroke", function(d){ return myColor(selectedGroup) })
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton2").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+
+})
+/*
+// set the dimensions and margins of the example graph
 var margin2 = {top: 10, right: 100, bottom: 30, left: 30},
     width2 = 660 - margin2.left - margin2.right,
     height2 = 600 - margin2.top - margin2.bottom;
