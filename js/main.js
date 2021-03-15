8aea4ae217a2de96f6f362f4a37454047c72409d
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-03-15 07:17:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,6 +21,36 @@ function step1() {
 });
 }
 
+// News Menu
+		var menuButton = document.querySelector('.menu-button');
+		var openMenu = function () {
+			swiper.slidePrev();
+		};
+		var swiper = new Swiper('.swiper-container', {
+			slidesPerView: 'auto',
+			initialSlide: 1,
+			resistanceRatio: 0,
+			slideToClickedSlide: true,
+			on: {
+				slideChangeTransitionStart: function () {
+					var slider = this;
+					if (slider.activeIndex === 0) {
+						menuButton.classList.add('cross');
+						// required because of slideToClickedSlide
+						menuButton.removeEventListener('click', openMenu, true);
+					} else {
+						menuButton.classList.remove('cross');
+					}
+				}
+				, slideChangeTransitionEnd: function () {
+					var slider = this;
+					if (slider.activeIndex === 1) {
+						menuButton.addEventListener('click', openMenu, true);
+					}
+				},
+			}
+		});
+
 //Covid Tracking Project Info
 covidtrackinfo.onmouseover = logMouseOver;
 covidtrackinfo.onmouseout = logMouseOut;
