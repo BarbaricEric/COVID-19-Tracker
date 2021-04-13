322ab9e25291bec8fcf9a3a4c6848a2a12134775
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-04-13 09:13:50 -0700

    Update cdcdatacollection.js

--- js/cdcdatacollection.js
+++ js/cdcdatacollection.js
@@ -34,6 +34,7 @@
 //US Fully Vaccinated Count from CDC
   d3v6.csv("./cdc_vaccination/vaccined_fully.csv").then(function(data) { 
    const vacNum = document.querySelector(".vac-num")
+   vacNum.style.cssText = "font-size:46px";
    vacNum.textContent = addCommas(parseInt(data[0].PfizerBioNTech) + parseInt(data[0].Moderna) + parseInt(data[0].Janssen) + parseInt(data[0].NotIdentified))
   });
 
