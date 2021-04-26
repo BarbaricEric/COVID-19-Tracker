99369bb0acaf205f444bd4e67210562981d101ba
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-04-26 07:58:40 -0700

    Update cdcdatacollection.js

--- js/cdcdatacollection.js
+++ js/cdcdatacollection.js
@@ -73,7 +73,7 @@
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + (margin.left + 50) + "," + (margin.top + 50) + ")");
+          "translate(" + (margin.left + 250) + "," + (margin.top + 250) + ")");
 
   svg.append("g")
       .attr("stroke", "white")
@@ -103,6 +103,170 @@
           .attr("fill-opacity", 0.7)
           .text(d => addCommas(d.data.value.toLocaleString())));
   });
+
+
+//Pfizer Deliever Chart data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
+
+   // set the dimensions and margins of the first graph
+var margin = {top: 10, right: 100, bottom: 30, left: 30},
+    width = 660 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+var svg = d3.select("#pfizer2")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  
+    // List of groups (here I have one group per column)
+    var allGroup = ["valueA", "valueB", "valueC"]
+
+    // add the options to the button
+    d3.select("#selectButton")
+      .selectAll('myOptions')
+     	.data(allGroup)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    // A color scale: one color for each group
+    var myColor = d3.scaleOrdinal()
+      .domain(allGroup)
+      .range(d3.schemeSet2);
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+      .domain([0,10])
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain( [0,20])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+      
+    //Add Title
+    var title = svg
+      .append("text")
+      .attr("x", (width / 2))   
+      .attr("y", margin.top)
+      //.attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "30px") 
+      .text("Pfizer and BioNTech Deliever Trend");
+      
+      // create a tooltip
+  var Tooltip = d3.select("#pfizer2")
+    .append("div")
+  //d3.tip()
+  //.offset([-10, 0])	
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave 
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1)
+      .style("stroke", "black")  
+    /*d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)*/
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html("<strong>The exact value of this point is:</strong> <span style='color:red'>" + d.value + "</span>" + "<br>" + d.time)  
+      .style("left", (d3.event.pageX) + "px")
+      .style("top", (d3.event.pageY - 28) + "px");
+      //.style("left", (d3.mouse(this)[0]+70) + "px")
+      //.style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+      .style("stroke", "none")
+    /*d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)*/
+  }
+
+    // Initialize line with group a
+    var line = svg
+      .append('g')
+      .append("path")
+        .datum(data)
+        .attr("d", d3.line()
+          //.x(function(d) { return x(+d.time) })
+          //.y(function(d) { return y(+d.valueA) })
+          .x(d => x(+d.time))
+          .y(d => y(+d.valueA))
+          .curve(d3.curveMonotoneX)    
+        )
+        .attr("stroke", function(d){ return myColor("valueA") })
+        .style("stroke-width", 4)
+        .style("fill", "none")
+     .on("mouseover", function(d) {
+       Tooltip.transition()
+         .duration(200)
+         .style("opacity", .9);
+       Tooltip.html(d.date + "<br/>" + d.close)
+         .style("left", (d3.event.pageX) + "px")
+         .style("top", (d3.event.pageY - 28) + "px");
+       })
+     .on("mouseout", function(d) {
+       Tooltip.transition()
+         .duration(500)
+         .style("opacity", 0);
+       });    
+
+    // A function that update the chart
+    function update(selectedGroup) {
+
+      // Create new data with the selection
+      var dataFilter = 
+          //data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+          data.map(d => ({time: d.time, value: d[selectedGroup]}))
+
+      // Give these new data to update line
+      line
+          .datum(dataFilter)
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()    
+            //.x(function(d) { return x(+d.time) })
+            //.y(function(d) { return y(+d.value) })
+            .x(d => x(+d.time))
+            .y(d => y(+d.value))
+            .curve(d3.curveMonotoneX)
+          )
+          .attr("stroke", function(d){ return myColor(selectedGroup) })
+          .call(mouseover)
+          .call(mousemove)
+          .call(mouseleave)
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+
+})
 
 //Pie Chart US Administereded Count from CDC
   d3v6.csv("./cdc_vaccination/vaccine_admin.csv").then(function(data) {
