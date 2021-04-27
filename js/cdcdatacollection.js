972436657bca162e9e53ac223d900acf810b8925
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-04-27 07:34:19 -0700

    Update cdcdatacollection.js

--- js/cdcdatacollection.js
+++ js/cdcdatacollection.js
@@ -268,6 +268,116 @@ var svg = d3.select("#pfizer2")
 
 })
 
+//Pfizer Deliever Chart data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
+
+  // set the dimensions and margins of the second graph
+var margin2 = {top: 10, right: 100, bottom: 30, left: 30},
+    width2 = 660 - margin2.left - margin2.right,
+    height2 = 600 - margin2.top - margin2.bottom;
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#test2")
+  .append("svg")
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin2.left + "," + margin2.top + ")");
+  
+  
+    // List of groups (here I have one group per column)
+    const allGroup = ["valueA", "valueB", "valueC"]
+
+    // add the options to the button
+    d3.select("#selectButton2")
+      .selectAll('myOptions')
+     	.data(allGroup)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    // A color scale: one color for each group
+    const myColor = d3.scaleOrdinal()
+      .domain(allGroup)
+      .range(d3.schemeSet2);
+
+    // Add X axis --> it is a date format
+    const x = d3.scaleLinear()
+      .domain([0,10])
+      .range([ 0, width2 ]);
+    svg2.append("g")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain( [0,20])
+      .range([ height2, 0 ]);
+    svg2.append("g")
+      .call(d3.axisLeft(y));
+      
+    //Add Title
+    const title = svg2
+      .append("text")
+      .attr("x", (width2 / 2))   
+      .attr("y", margin2.top)
+      //.attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "30px")  
+      .text("Moderna Delievery Trend");
+      
+    // Initialize line with group a
+    const line = svg2
+      .append('g')
+      .append("path")
+        .datum(data)
+        .attr("d", d3.line()
+          //.x(function(d) { return x(+d.time) })
+          //.y(function(d) { return y(+d.valueA) })
+          .x(d => x(+d.time))
+          .y(d => y(+d.valueA))
+          .curve(d3.curveMonotoneX)    
+        )
+        .attr("stroke", function(d){ return myColor("valueA") })
+        .style("stroke-width", 4)
+        .style("fill", "none")
+
+    // A function that update the chart
+    function update(selectedGroup) {
+
+      // Create new data with the selection
+      const dataFilter = 
+          //data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+          data.map(d => ({time: d.time, value: d[selectedGroup]}))
+
+      // Give these new data to update line
+      line
+          .datum(dataFilter)
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()    
+            //.x(function(d) { return x(+d.time) })
+            //.y(function(d) { return y(+d.value) })
+            .x(d => x(+d.time))
+            .y(d => y(+d.value))
+            .curve(d3.curveMonotoneX)
+          )
+          .attr("stroke", function(d){ return myColor(selectedGroup) })
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton2").on("change", function(d) {
+        // recover the option that has been chosen
+        const selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+
+})
+
+
 //Pie Chart US Administereded Count from CDC
   d3v6.csv("./cdc_vaccination/vaccine_admin.csv").then(function(data) {
    console.log(data); 
