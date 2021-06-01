1bbbfc1197d824aa20d84653537f6e05f83a85e0
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-06-01 08:36:26 -0700

    Update news_container.js

--- js/news_container.js
+++ js/news_container.js
@@ -4,17 +4,14 @@ const e = React.createElement;
 
 class Hello extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("div", null, "Hello ", this.props.toWhat), 
-      /*#__PURE__*/React.createElement("div", {
-      id: "hhello",
-      class: "dif3"
-    }, "Hello");
+    return e("div", null, "Hello ", this.props.toWhat), 
+      e("div", {id: "hhello", class: "dif3"}, "Hello");
   }
 }
 
 ReactDOM.render(
   e(Hello, {toWhat: "World"}), 
-  document.getElementById('root')
+  document.getElementById('article-main')
 );
 
 /* https://reactjs.org/docs/react-without-jsx.html */
