b79f3160cf6e7df490d8d0f28d086186ae13c54a
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-06-02 07:05:32 -0700

    Update news_container.js

--- js/news_container.js
+++ js/news_container.js
@@ -9,12 +9,12 @@ class Hello extends React.Component {
   }
 }
 
-/*class NewsList extends React.Component {
+class NewsList extends React.Component {
   render() {
     return e("div", {class: "article-container"},
     e("div", {class: "news-box news-box-a"},
     e("i", {class: "fas fa-exclamation-circle fa-2x"}, this.props.newsType),
-    e("h3", null, this.props.newsStory), 
+    e("h3", null, this.props.newsTitle), 
     e("p", {class: "price"}, "Source: ", this.props.newsSource, ", Published on ", this.props.newsDate), 
     e("a", {href: "{this.props.newsLink}", class: "btn"}, this.props.newlinkTitle)),
     e("div", {class: "news-box news-box-b"},
@@ -23,9 +23,9 @@ class Hello extends React.Component {
 }
 
 ReactDOM.render(
-  e(Hello, {toWhat: "World"}),
-  e(NewsList, {this.props.newsType:}, {this.props.newsStory:}, {this.props.newsSource:}, {this.props.newsDate:}, {this.props.newsLink:}, {this.props.newlinkTitle):},
-  document.getElementById('article-main')
+  e(NewsList, {this.props.newsType:"Media News"}, {this.props.newsTitle:"Moderna Announces Initiation of Rolling Submission of Biologics License Application (BLA) with U.S. FDA for the Moderna COVID-19 Vaccine"}, {this.props.newsSource:"Moderna"}, {this.props.newsDate:"June 1, 2021"}, {this.props.newsLink:"https://investors.modernatx.com/news-releases/news-release-details/moderna-announces-initiation-rolling-submission-biologics"}, {this.props.newlinkTitle):"Link to Article"},
+  e(NewsList, {this.props.newsType:"1"}, {this.props.newsTitle:"2"}, {this.props.newsSource:"3"}, {this.props.newsDate:"4"}, {this.props.newsLink:"https://investors.modernatx.com/news-releases/news-release-details/moderna-announces-initiation-rolling-submission-biologics"}, {this.props.newlinkTitle):"7"},
+  document.getElementById('article-main-june2021')
 );
 
- https://reactjs.org/docs/react-without-jsx.html */
+/* https://reactjs.org/docs/react-without-jsx.html */
