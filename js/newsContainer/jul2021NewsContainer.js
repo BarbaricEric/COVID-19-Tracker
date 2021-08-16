46efe7d5b7f7e63eefacd84ba2ad77dca58124d2
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-16 08:18:38 -0700

    Update jul2021NewsContainer.js

--- js/newsContainer/jul2021NewsContainer.js
+++ js/newsContainer/jul2021NewsContainer.js
@@ -5,7 +5,8 @@ const e = React.createElement;
 class July2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
-         e(NewsList, {newsType: "Media News", newsTitle: "", newsSource: "", newsDate: "", newsLink: ""})
+e(NewsList, {newsType: "Non Media News", newsTitle: "Estimating the early impact of vaccination against COVID-19 on deaths among elderly people in Brazil: Analyses of routinely-collected data on vaccine coverage and mortality", newsSource: "Lancet", newsDate: "July 15, 2021", newsLink: "https://doi.org/10.1016/j.eclinm.2021.101036"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Cross-reactive antibodies against human coronaviruses and the animal coronavirome suggest diagnostics for future zoonotic spillovers", newsSource: "Science Magazine", newsDate: "July 29, 2021", newsLink: "https://doi.org/10.1126/sciimmunol.abe9950"}),
          ); 
  }
 }
