b6db03b240927514c82ddaaf9b4c84a9a0a22a80
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-16 08:23:43 -0700

    Update augNewsContainer.js

--- js/newsContainer/augNewsContainer.js
+++ js/newsContainer/augNewsContainer.js
@@ -2,6 +2,21 @@
 
 const e = React.createElement;
 
+class NewsList extends React.Component {
+  render() {
+    return e("div", {class: "article-container"},
+    e("div", {class: "news-box news-box-a"},
+    e("i", {class: "fas fa-exclamation-circle fa-2x"}, this.props.newsType),
+    e("h3", null, this.props.newsTitle), 
+    e("p", {class: "price"}, "Source: ", this.props.newsSource, ", Published on ", this.props.newsDate), 
+    e("a", {target: "_blank", href: `${this.props.newsLink}`, class: "btn"}, "Link to Article")),  
+    e("div", {class: "news-box news-box-b"}, 
+    e("img", {src: "img/coronavirus-19-mobile.jpg",alt: "COVID-19 News Image"
+    }))
+    );
+  }
+}
+
 class August2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
@@ -35,7 +50,7 @@ e(NewsList, {newsType: "Media News", newsTitle: "‘I feel defeated’: Mask and
 
 document.addEventListener("DOMContentLoaded", function(event) {
   ReactDOM.render(
-  e(NewsApp), 
+  e(August2021NewsApp), 
   //e(Welcome, {name: "Person"}), e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "August , 2021", newsLink: ""}),
   document.querySelector('#article-main-aug2021')
   );
