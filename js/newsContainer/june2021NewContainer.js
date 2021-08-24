3079a64705670f4d200d0db42ac2144c5a8e300b
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-24 10:40:00 -0700

    Update june2021NewContainer.js

--- js/newsContainer/june2021NewContainer.js
+++ js/newsContainer/june2021NewContainer.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const e = React.createElement;
-
 class June2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
 e(NewsList, {newsType: "Non Media News", newsTitle: "Socioeconomic disparities and COVID-19 vaccination acceptance: a nationwide ecologic study", newsSource: "Science Direct", newsDate: "June 7, 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S1198743X21002779"}),
-e(NewsList, {newsType: "Non Media News", newsTitle: "Polθ reverse transcribes RNA and promotes RNA-templated DNA repair", newsSource: "Science Magazine", newsDate: "June 11, 2021", newsLink: "https://doi.org/10.1126/sciadv.abf1771"}),               
+e(NewsList, {newsType: "Non Media News", newsTitle: "Polθ reverse transcribes RNA and promotes RNA-templated DNA repair", newsSource: "Science Magazine", newsDate: "June 11, 2021", newsLink: "https://doi.org/10.1126/sciadv.abf1771"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Association between COVID-19 outcomes and mask mandates, adherence, and attitudes", newsSource: "PLOS", newsDate: "June 23, 2021", newsLink: "https://doi.org/10.1371/journal.pone.0252315"}),               
          ); 
  }
 }
