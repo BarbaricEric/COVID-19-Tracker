7e5dff17d7dd1b98262e467cfee327a67abe6762
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-27 10:13:55 -0700

    Update june2021NewContainer.js

--- js/newsContainer/june2021NewContainer.js
+++ js/newsContainer/june2021NewContainer.js
@@ -4,6 +4,7 @@ class June2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
 e(NewsList, {newsType: "Non Media News", newsTitle: "Socioeconomic disparities and COVID-19 vaccination acceptance: a nationwide ecologic study", newsSource: "Science Direct", newsDate: "June 7, 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S1198743X21002779"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "First report of a de novo iTTP episode associated with an mRNA-based anti-COVID-19 vaccination", newsSource: "Wiley Online Library", newsDate: "June 8, 2021", newsLink: "https://doi.org/10.1111/jth.15418"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Polθ reverse transcribes RNA and promotes RNA-templated DNA repair", newsSource: "Science Magazine", newsDate: "June 11, 2021", newsLink: "https://doi.org/10.1126/sciadv.abf1771"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "Manipulation of the unfolded protein response: A pharmacological strategy against coronavirus infection", newsSource: "PLOS", newsDate: "June 17, 2021", newsLink: "https://doi.org/10.1371/journal.ppat.1009644"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Association between COVID-19 outcomes and mask mandates, adherence, and attitudes", newsSource: "PLOS", newsDate: "June 23, 2021", newsLink: "https://doi.org/10.1371/journal.pone.0252315"}),               
