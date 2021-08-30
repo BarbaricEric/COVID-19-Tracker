5076d3971a4b87d0d44e96937dbc6af789af2d4b
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-30 10:02:14 -0700

    Update may2021NewsContainer.js

--- js/newsContainer/may2021NewsContainer.js
+++ js/newsContainer/may2021NewsContainer.js
@@ -5,7 +5,8 @@ class May2021NewsApp extends React.Component {
       return e("div", null,
 e(NewsList, {newsType: "Non Media News", newsTitle: "Shared B cell memory to coronaviruses and other pathogens varies in human age groups and tissues", newsSource: "Science Magazine", newsDate: "May 14, 2021", newsLink: "https://doi.org/10.1126/science.abf6648"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "Mask Use and Ventilation Improvements to Reduce COVID-19 Incidence in Elementary Schools — Georgia, November 16–December 11, 2020", newsSource: "CDC", newsDate: "May 21, 2021", newsLink: "http://dx.doi.org/10.15585/mmwr.mm7021e1"}),               
-e(NewsList, {newsType: "Non Media News", newsTitle: "Hemodynamic Profiles of Shock in Patients With COVID-19", newsSource: "American Journal of Cardiology", newsDate: "May 25, 2021", newsLink: "https://doi.org/10.1016/j.amjcard.2021.05.029"}),              
+e(NewsList, {newsType: "Non Media News", newsTitle: "Hemodynamic Profiles of Shock in Patients With COVID-19", newsSource: "American Journal of Cardiology", newsDate: "May 25, 2021", newsLink: "https://doi.org/10.1016/j.amjcard.2021.05.029"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Doing it for us: Community identification predicts willingness to receive a COVID-19 vaccination via perceived sense of duty to the community", newsSource: "Wiley Online Library", newsDate: "May 25, 2021", newsLink: "https://doi.org/10.1002/casp.2542"}),               
          ); 
  }
 }
