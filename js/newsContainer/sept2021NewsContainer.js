51ead0bd7bb0025461a527649f54fb62dc6f8113
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-30 10:30:12 -0700

    Update sept2021NewsContainer.js

--- js/newsContainer/sept2021NewsContainer.js
+++ js/newsContainer/sept2021NewsContainer.js
@@ -3,7 +3,7 @@
 class September2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
-e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "September, 2021", newsLink: ""}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Common factors of COVID-19 cases and deaths among the most affected 50 countries", newsSource: "Science Direct", newsDate: "September 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S1871402121002678"}),
 e(NewsList, {newsType: "Media News", newsTitle: "", newsSource: "", newsDate: "September, 2021", newsLink: ""}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "September, 2021", newsLink: ""}),
 e(NewsList, {newsType: "Media News", newsTitle: "", newsSource: "", newsDate: "September, 2021", newsLink: ""}),
