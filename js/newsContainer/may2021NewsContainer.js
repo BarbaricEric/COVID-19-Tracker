0afae7ba888027511a6ad0c99e1ebdbe52d9e5ad
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-08-30 07:27:34 -0700

    Update may2021NewsContainer.js

--- js/newsContainer/may2021NewsContainer.js
+++ js/newsContainer/may2021NewsContainer.js
@@ -1,26 +1,10 @@
 'use strict';
 
-const e = React.createElement;
-
-class NewsList extends React.Component {
-  render() {
-    return e("div", {class: "article-container"},
-    e("div", {class: "news-box news-box-a"},
-    e("i", {class: "fas fa-exclamation-circle fa-2x"}, this.props.newsType),
-    e("h3", null, this.props.newsTitle), 
-    e("p", {class: "price"}, "Source: ", this.props.newsSource, ", Published on ", this.props.newsDate), 
-    e("a", {target: "_blank", href: `${this.props.newsLink}`, class: "btn"}, "Link to Article")),  
-    e("div", {class: "news-box news-box-b"}, 
-    e("img", {src: "img/coronavirus-19-mobile.jpg",alt: "COVID-19 News Image"
-    }))
-    );
-  }
-}
-
 class May2021NewsApp extends React.Component {
  render() {
       return e("div", null,
-e(NewsList, {newsType: "Non Media News", newsTitle: "Shared B cell memory to coronaviruses and other pathogens varies in human age groups and tissues", newsSource: "Science Magazine", newsDate: "May 14, 2021", newsLink: "https://doi.org/10.1126/science.abf6648"}),               
+e(NewsList, {newsType: "Non Media News", newsTitle: "Shared B cell memory to coronaviruses and other pathogens varies in human age groups and tissues", newsSource: "Science Magazine", newsDate: "May 14, 2021", newsLink: "https://doi.org/10.1126/science.abf6648"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Mask Use and Ventilation Improvements to Reduce COVID-19 Incidence in Elementary Schools — Georgia, November 16–December 11, 2020", newsSource: "CDC", newsDate: "May 21, 2021", newsLink: "http://dx.doi.org/10.15585/mmwr.mm7021e1"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Hemodynamic Profiles of Shock in Patients With COVID-19", newsSource: "American Journal of Cardiology", newsDate: "May 25, 2021", newsLink: "https://doi.org/10.1016/j.amjcard.2021.05.029"}),              
          ); 
  }
