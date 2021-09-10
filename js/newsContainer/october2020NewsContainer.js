f22bef601633931cdcd1767969391f019717b7f8
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-09-10 10:44:57 -0700

    Update october2020NewsContainer.js

--- js/newsContainer/october2020NewsContainer.js
+++ js/newsContainer/october2020NewsContainer.js
@@ -1,24 +1,10 @@
 'use strict';
 
-class NewsList extends React.Component {
-  render() {
-    return e("div", {class: "article-container"},
-    e("div", {class: "news-box news-box-a"},
-    e("i", {class: "fas fa-exclamation-circle fa-2x"}, this.props.newsType),
-    e("h3", null, this.props.newsTitle), 
-    e("p", {class: "price"}, "Source: ", this.props.newsSource, ", Published on ", this.props.newsDate), 
-    e("a", {target: "_blank", href: `${this.props.newsLink}`, class: "btn"}, "Link to Article")),  
-    e("div", {class: "news-box news-box-b"}, 
-    e("img", {src: "img/coronavirus-19-mobile.jpg",alt: "COVID-19 News Image"
-    }))
-    );
-  }
-}
-
 class October2020NewsApp extends React.Component {
  render() {
       return e("div", null, 
 e(NewsList, {newsType: "Non Media News", newsTitle: "Enhancing innate immunity against virus in times of COVID-19: Trying to untangle facts from fictions", newsSource: "World Allergy Organization Journal", newsDate: "October 9, 2020", newsLink: "https://doi.org/10.1016/j.waojou.2020.100476"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Inflammation and Intussusceptive Angiogenesis in COVID-19: everything in and out of Flow", newsSource: "European Respiratory Society", newsDate: "October 20, 2020", newsLink: "https://doi.org/10.1183/13993003.03147-2020"}),               
          ); 
  }
 }
@@ -26,7 +12,7 @@ e(NewsList, {newsType: "Non Media News", newsTitle: "Enhancing innate immunity a
 document.addEventListener("DOMContentLoaded", function(event) {
   ReactDOM.render(
   e(October2020NewsApp), 
-  //e(Welcome, {name: "Person"}), e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "August , 2021", newsLink: ""}),
+  //e(Welcome, {name: "Person"}), e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "October , 2020", newsLink: ""}),
   document.querySelector('#article-main-october2020')
   );
 });
