e7247ea3ae5215c1e39432458cf662609ae90cfa
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-09-14 10:14:10 -0700

    Update april2021NewsContainer.js

--- js/newsContainer/april2021NewsContainer.js
+++ js/newsContainer/april2021NewsContainer.js
@@ -5,7 +5,8 @@ class April2021NewsApp extends React.Component {
       return e("div", null,
 e(NewsList, {newsType: "Media News", newsTitle: "How coronavirus spreads through a population and how we can beat it", newsSource: "Guardian", newsDate: "April 2020", newsLink: "https://www.theguardian.com/world/datablog/ng-interactive/2021/sep/06/how-contagious-delta-variant-covid-19-r0-r-factor-value-number-explainer-see-how-coronavirus-spread-infectious-flatten-the-curve"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "FDA Revokes Emergency Use Authorization for Monoclonal Antibody Bamlanivimab", newsSource: "U.S. Food and Drug Administration", newsDate: "April 16, 2021", newsLink: "https://www.fda.gov/news-events/press-announcements/coronavirus-covid-19-update-fda-revokes-emergency-use-authorization-monoclonal-antibody-bamlanivimab"}),               
-e(NewsList, {newsType: "Non Media News", newsTitle: "Humoral immune response to COVID-19 mRNA vaccine in patients with multiple sclerosis treated with high-efficacy disease-modifying therapies", newsSource: "Journal Sage", newsDate: "April 22, 2021", newsLink: "https://doi.org/10.1177/17562864211012835"}),               
+e(NewsList, {newsType: "Non Media News", newsTitle: "Humoral immune response to COVID-19 mRNA vaccine in patients with multiple sclerosis treated with high-efficacy disease-modifying therapies", newsSource: "Journal Sage", newsDate: "April 22, 2021", newsLink: "https://doi.org/10.1177/17562864211012835"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Multiscale statistical physics of the pan-viral interactome unravels the systemic nature of SARS-CoV-2 infections", newsSource: "Nature", newsDate: "April 23, 2021", newsLink: "https://doi.org/10.1038/s42005-021-00582-8"}),               
          ); 
  }
 }
