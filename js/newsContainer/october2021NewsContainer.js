b9387d36793973a277eb1817fc26f50ac2bbac3e
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-09-15 09:51:08 -0700

    Update october2021NewsContainer.js

--- js/newsContainer/october2021NewsContainer.js
+++ js/newsContainer/october2021NewsContainer.js
@@ -1,24 +1,10 @@
 'use strict';
 
-class NewsList extends React.Component {
-  render() {
-    return e("div", {class: "article-container"},
-    e("div", {class: "news-box news-box-a"},
-    e("i", {class: "fas fa-exclamation-circle fa-2x"}, this.props.newsType),
-    e("h3", null, this.props.newsTitle), 
-    e("p", {class: "price"}, "Source: ", this.props.newsSource, ", Published on ", this.props.newsDate), 
-    e("a", {target: "_blank", href: `${this.props.newsLink}`, class: "btn"}, "Link to Article")),  
-    e("div", {class: "news-box news-box-b"}, 
-    e("img", {src: "img/coronavirus-19-mobile.jpg",alt: "COVID-19 News Image"
-    }))
-    );
-  }
-}
-
 class October2021NewsApp extends React.Component {
  render() {
       return e("div", null, 
 e(NewsList, {newsType: "Non Media News", newsTitle: "Anxiety, depression, insomnia, and trauma-related symptoms following COVID-19 infection at long-term follow-up", newsSource: "Science Direct", newsDate: "October 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S2666354621001186"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Human exposure to respiratory aerosols in a ventilated room: Effects of ventilation condition, emission mode, and social distancing", newsSource: "Science Direct", newsDate: "October 2021", newsLink: "https://www.sciencedirect.com/science/article/abs/pii/S2210670721003735"}),               
          ); 
  }
 }
