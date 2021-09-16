c4784746a73c25074246f84305fcc4d6b733f697
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-09-16 10:45:58 -0700

    Update march2021NewsContainer.js

--- js/newsContainer/march2021NewsContainer.js
+++ js/newsContainer/march2021NewsContainer.js
@@ -3,8 +3,10 @@
 class March2021NewsApp extends React.Component {
  render() {
       return e("div", null,
+e(NewsList, {newsType: "Non Media News", newsTitle: "Association Between ABO and Rh Blood Groups and SARS-CoV-2 Infection or Severe COVID-19 Illness", newsSource: "ACP Journals", newsDate: "March 2021", newsLink: "https://doi.org/10.7326/M20-4511"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "Highly functional virus-specific cellular immune response in asymptomatic SARS-CoV-2 infection", newsSource: "Journal of Experimental Medicine", newsDate: "March 1, 2021", newsLink: "https://doi.org/10.1084/jem.20202617"}),               
-); 
+e(NewsList, {newsType: "Non Media News", newsTitle: "Experimental SARS-CoV-2 Infection of Bank Voles", newsSource: "CDC", newsDate: "March 11, 2021", newsLink: "https://doi.org/10.3201/eid2704.204945"}),
+              ); 
  }
 }
 
