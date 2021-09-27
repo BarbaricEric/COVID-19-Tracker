9590e6466680b8d1d7bc57121e9b91f3e0d7f1ac
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-09-27 05:58:31 -0700

    Update october2021NewsContainer.js

--- js/newsContainer/october2021NewsContainer.js
+++ js/newsContainer/october2021NewsContainer.js
@@ -2,7 +2,8 @@
 
 class October2021NewsApp extends React.Component {
  render() {
-      return e("div", null, 
+      return e("div", null,
+e(NewsList, {newsType: "Non Media News", newsTitle: "The combined treatment of Molnupiravir and Favipiravir results in a potentiation of antiviral efficacy in a SARS-CoV-2 hamster infection model", newsSource: "Science Direct", newsDate: "October 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S2352396421003881"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Anxiety, depression, insomnia, and trauma-related symptoms following COVID-19 infection at long-term follow-up", newsSource: "Science Direct", newsDate: "October 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S2666354621001186"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "Human exposure to respiratory aerosols in a ventilated room: Effects of ventilation condition, emission mode, and social distancing", newsSource: "Science Direct", newsDate: "October 2021", newsLink: "https://www.sciencedirect.com/science/article/abs/pii/S2210670721003735"}),               
          ); 
