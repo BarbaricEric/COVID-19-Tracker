43aad24b64f193157f59d3c89207ef3f53c29ad7
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-10-03 09:02:36 -0700

    Create november2021NewsContainer.js

---
+++ js/newsContainer/november2021NewsContainer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class November2021NewsApp extends React.Component {
+ render() {
+      return e("div", null,
+e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "October , 2021", newsLink: ""}),
+              ); 
+ }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  ReactDOM.render(
+  e(November2021NewsApp), 
+  //e(Welcome, {name: "Person"}), e(NewsList, {newsType: "Non Media News", newsTitle: "", newsSource: "", newsDate: "October , 2021", newsLink: ""}),
+  document.querySelector('#article-main-november2021')
+  );
+});
