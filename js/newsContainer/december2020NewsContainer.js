e4a3b46ff4414c6fd3a40d1e03c3dcaf6d3e6750
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-10-22 09:56:07 -0700

    Update december2020NewsContainer.js

--- js/newsContainer/december2020NewsContainer.js
+++ js/newsContainer/december2020NewsContainer.js
@@ -3,6 +3,7 @@
 class December2020NewsApp extends React.Component {
  render() {
       return e("div", null,
+e(NewsList, {newsType: "Non Media News", newsTitle: "Male sex identified by global COVID-19 meta-analysis as a risk factor for death and ITU admission", newsSource: "Nature", newsDate: "December 9, 2020", newsLink: "https://doi.org/10.1038/s41467-020-19741-6"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Susceptibility of Domestic Swine to Experimental Infection with Severe Acute Respiratory Syndrome Coronavirus 2", newsSource: "CDC", newsDate: "December 18, 2020", newsLink: "https://doi.org/10.3201/eid2701.203399"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Coronavirus vaccine development: from SARS and MERS to COVID-19", newsSource: "BMC", newsDate: "December 20, 2020", newsLink: "https://doi.org/10.1186/s12929-020-00695-2"}),
          ); 
