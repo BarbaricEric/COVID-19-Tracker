5d22d5fa968546eda6511eab161c2de7a60a9d2f
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2021-10-29 09:54:07 -0700

    Update jul2021NewsContainer.js

--- js/newsContainer/jul2021NewsContainer.js
+++ js/newsContainer/jul2021NewsContainer.js
@@ -25,7 +25,8 @@ e(NewsList, {newsType: "Non Media News", newsTitle: "Cross-reactive antibodies a
 e(NewsList, {newsType: "Non Media News", newsTitle: "Intranasal vaccines for SARS-CoV-2: From challenges to potential in COVID-19 management", newsSource: "Science Direct", newsDate: "July 29, 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S1359644621003317"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Drug-induced phospholipidosis confounds drug repurposing for SARS-CoV-2", newsSource: "Science Magazine", newsDate: "July 30, 2021", newsLink: "https://doi.org/10.1126/science.abi4708"}),
 e(NewsList, {newsType: "Non Media News", newsTitle: "A cluster of the new SARS-CoV-2 B.1.621 lineage in Italy and sensitivity of the viral isolate to the BNT162b2 vaccine", newsSource: "Wiley Online Library", newsDate: "July 30, 2021", newsLink: "https://doi.org/10.1002/jmv.27247"}),               
-e(NewsList, {newsType: "Non Media News", newsTitle: "The association of COVID-19 infection in pregnancy with preterm birth: A retrospective cohort study in California", newsSource: "Science Direct", newsDate: "July 30, 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S2667193X21000193"}),               
+e(NewsList, {newsType: "Non Media News", newsTitle: "The association of COVID-19 infection in pregnancy with preterm birth: A retrospective cohort study in California", newsSource: "Science Direct", newsDate: "July 30, 2021", newsLink: "https://www.sciencedirect.com/science/article/pii/S2667193X21000193"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Outbreak of SARS-CoV-2 Infections, Including COVID-19 Vaccine Breakthrough Infections, Associated with Large Public Gatherings — Barnstable County, Massachusetts, July 2021", newsSource: "CDC", newsDate: "July 30, 2021", newsLink: "http://dx.doi.org/10.15585/mmwr.mm7031e2"}),               
          ); 
  }
 }
