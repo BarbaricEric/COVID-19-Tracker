25b73e3c1d46bf6644107233b216af808c45d3b6
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2022-01-04 08:54:45 -0800

    Update october2020NewsContainer.js

--- js/newsContainer/october2020NewsContainer.js
+++ js/newsContainer/october2020NewsContainer.js
@@ -4,6 +4,7 @@ class October2020NewsApp extends React.Component {
  render() {
       return e("div", null, 
 e(NewsList, {newsType: "Non Media News", newsTitle: "Enhancing innate immunity against virus in times of COVID-19: Trying to untangle facts from fictions", newsSource: "World Allergy Organization Journal", newsDate: "October 9, 2020", newsLink: "https://doi.org/10.1016/j.waojou.2020.100476"}),
+e(NewsList, {newsType: "Non Media News", newsTitle: "Facial Masking for Covid-19 — Potential for “Variolation” as We Await a Vaccine", newsSource: "New England Journal of Medicine", newsDate: "October 20, 2020", newsLink: "https://www.nejm.org/doi/full/10.1056/nejmp2026913"}),               
 e(NewsList, {newsType: "Non Media News", newsTitle: "Inflammation and Intussusceptive Angiogenesis in COVID-19: everything in and out of Flow", newsSource: "European Respiratory Society", newsDate: "October 20, 2020", newsLink: "https://doi.org/10.1183/13993003.03147-2020"}),               
          ); 
  }
